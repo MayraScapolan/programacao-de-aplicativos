4664cdea2005b69b41afb504575dd0d02b30940d
mayra <mayra.scapolan2010@gmail.com>
2026-05-20 12:39:25 -0300

    auladada

---
+++ escoladigital.py
@@ -0,0 +1,20 @@
+import json
+
+def criar_arquivo():
+    with open("escoladigital.json" , 'w').close()
+criar_araquivo()
+
+ cpf_aluno int(input("digite seu cpf: "))
+ nome_completo (input("digite o seu nome completo: "))
+ telefone_aluno int(input("digite o seu telefone: "))
+ turma_aluno int(input("digite o sua turma: "))
+ idade_aluno int(input("digite a sua idade: "))
+
+
+ aluno=    {"cpf" : cpf_aluno
+           "nome" : nome_completo
+           "telefone" : telefone_aluno
+           "turma" : turma_aluno
+           "idade" : idade_aluno
+
+ }
