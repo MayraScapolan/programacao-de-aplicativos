761d2ecd4bbbd1d4718d5e991518037d0303f072
mayra <mayra.scapolan2010@gmail.com>
2026-06-10 12:02:03 -0300

    auladada

---
+++ cadastro.py
@@ -0,0 +1,30 @@
+import sqlite3
+
+conexao = sqlite3.connect('escola.db')
+cursor = conexao.cursor()
+cursor.execute('''
+      CREATE TABLE IF NOT EXISTS alunos (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        nome TEXT NOT NULL,
+        telefone TEXT,
+        turma TEXT,
+        idade INTEGER,
+        cpf TEXT UNIQUE NOT NULL
+    )''')
+
+nome_aluno = input("NOME: ")
+telefone_aluno = input("TELEFONE: ")
+turma_aluno = input("TURMA: ")
+idade_aluno = int(input("IDADE: "))
+CPF_aluno = input("CPF: ")
+
+comando_inserir = (f'''
+                        insert  into alunos (nome, telefone, idade,cpf)
+                        values('{nome_aluno}','{telefone_aluno}','{turma_aluno}',{idade_aluno},'{CPF_aluno}' )
+                        ''')
+                        
+cursor.execute(comando_inserir)
+conexão.commit()
+conexão.close 
+
+ 
