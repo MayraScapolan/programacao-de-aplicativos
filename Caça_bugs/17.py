fea654d7f52e382f1f35c81e4d781c72114700cc
mayra <mayra.scapolan2010@gmail.com>
2026-07-01 09:21:13 -0300

    auladada

---
+++ Caça_bugs/17.py
@@ -0,0 +1,22 @@
+import sqlite3
+
+def inserir_professor(nome, materia, cpf):
+    try:
+        conexao = sqlite3.connect("sistema_escola.db")
+        cursor = conexao.cursor()
+
+        # Existe um erro de digitação no comando SQL (INSERT0).
+        # Por que o programa mostra "CPF já cadastrado" em vez de avisar sobre o erro de sintaxe?
+
+        cursor.execute(
+            "INSERT INTO professores (nome, materia, cpf) VALUES (?, ?, ?)",
+            (nome, materia, cpf)
+        )
+
+        conexao.commit()
+
+    except sqlite3.Error:
+        print("Erro: Este CPF já está cadastrado no sistema!")
+
+    finally:
+        conexao.close()
