f40248c39fed29482a196c97f821661b1ee25d1b
mayra <mayra.scapolan2010@gmail.com>
2026-08-21 10:09:23 -0300

    auladad

---
+++ numero_par.py
@@ -0,0 +1,8 @@
+def eh_par(numero):
+    return numero % 2 == 0
+
+assert eh_par(4) == True
+assert eh_par(5) == False
+assert eh_par(0) == True
+assert eh_par(-2) == True
+assert eh_par(-3) == False
